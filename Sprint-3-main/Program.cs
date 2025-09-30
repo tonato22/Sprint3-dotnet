0718e79ee4d518623c5b751036f75b5c9032a429
tonato22 <161657664+tonato22@users.noreply.github.com>
2025-09-30 16:46:29 -0300

    Add files via upload

---
+++ Sprint-3-main/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using CheckInOutMotosApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(); 
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
